3897cda4b02b2b89d482f73bf15f8a63a0959334
Aaron Parker <aaron@stealthpuppy.com>
2019-05-13 23:13:55 +1000

    Pre-deploy

--- tests/Tests.ps1
+++ tests/Tests.ps1
@@ -14,3 +14,6 @@ Else {
 $res = Invoke-Pester -Path "$projectRoot\tests" -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru
 (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
 If ($res.FailedCount -gt 0) { Throw "$($res.FailedCount) tests failed." }
+
+# Line break for readability in AppVeyor console
+Write-Host -Object ''

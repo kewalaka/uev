7f6042ebc95fe139d9c0cca3235e491485606616
Aaron Parker <aaron@stealthpuppy.com>
2019-05-13 22:19:15 +1000

    Fix storage_account_name; add install

---
+++ tests/Install.ps1
@@ -0,0 +1,21 @@
+<#
+    .SYNOPSIS
+        AppVeyor tests setup script.
+#>
+# AppVeyor Testing
+If (Test-Path 'env:APPVEYOR_BUILD_FOLDER') {
+    $projectRoot = $env:APPVEYOR_BUILD_FOLDER
+}
+Else {
+    # Local Testing 
+    $projectRoot = ((Get-Item (Split-Path -Parent -Path $MyInvocation.MyCommand.Definition)).Parent).FullName
+}
+
+# Line break for readability in AppVeyor console
+Write-Host -Object ''
+Write-Host "PowerShell Version:" $PSVersionTable.PSVersion.tostring()
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name Pester -SkipPublisherCheck -Force
+Install-Module -Name PSScriptAnalyzer -SkipPublisherCheck -Force
+Install-Module -Name posh-git -Force

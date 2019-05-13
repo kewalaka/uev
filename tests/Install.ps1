492ce571c3b541613641f1ef52ed186b0d2f372c
Aaron Parker <aaron@stealthpuppy.com>
2019-05-13 23:44:12 +1000

    Fixing git push

--- tests/Install.ps1
+++ tests/Install.ps1
@@ -20,13 +20,3 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name Pester -SkipPublisherCheck -Force
 Install-Module -Name PSScriptAnalyzer -SkipPublisherCheck -Force
 Install-Module -Name posh-git -Force
-
-# Configure the git environment
-$env:Path += ";$env:ProgramFiles\Git\cmd"
-Import-Module posh-git -ErrorAction Stop
-git config --global credential.helper store
-Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:GitHubKey):x-oauth-basic@github.com`n"
-git config --global user.email "aaron@stealthpuppy.com"
-git config --global user.name "Aaron Parker"
-git config --global core.autocrlf true
-git config --global core.safecrlf false
--- tests/Pre-Deploy.ps1
+++ tests/Pre-Deploy.ps1
@@ -14,46 +14,6 @@ Else {
 # Line break for readability in AppVeyor console
 Write-Host -Object ''
 
-Write-Host ""
-Write-Host "$env:APPVEYOR"
-Write-Host "$env:APPVEYOR_API_URL"
-Write-Host "$env:APPVEYOR_ACCOUNT_NAME"
-Write-Host "$env:APPVEYOR_PROJECT_ID"
-Write-Host "$env:APPVEYOR_PROJECT_NAME"
-Write-Host "$env:APPVEYOR_PROJECT_SLUG"
-Write-Host "$env:APPVEYOR_BUILD_FOLDER"
-Write-Host "$env:APPVEYOR_BUILD_ID"
-Write-Host "$env:APPVEYOR_BUILD_NUMBER"
-Write-Host "$env:APPVEYOR_BUILD_VERSION"
-Write-Host "$env:APPVEYOR_BUILD_WORKER_IMAGE"
-Write-Host "$env:APPVEYOR_PULL_REQUEST_NUMBER"
-Write-Host "$env:APPVEYOR_PULL_REQUEST_TITLE"
-Write-Host "$env:APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME"
-Write-Host "$env:APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH"
-Write-Host "$env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT"
-Write-Host "$env:APPVEYOR_JOB_ID"
-Write-Host "$env:APPVEYOR_JOB_NAME"
-Write-Host "$env:APPVEYOR_JOB_NUMBER"
-Write-Host "$env:APPVEYOR_REPO_PROVIDER"
-Write-Host "$env:APPVEYOR_REPO_SCM"
-Write-Host "$env:APPVEYOR_REPO_NAME"
-Write-Host "$env:APPVEYOR_REPO_BRANCH"
-Write-Host "$env:APPVEYOR_REPO_TAG"
-Write-Host "$env:APPVEYOR_REPO_TAG_NAME"
-Write-Host "$env:APPVEYOR_REPO_COMMIT"
-Write-Host "$env:APPVEYOR_REPO_COMMIT_AUTHOR"
-Write-Host "$env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL"
-Write-Host "$env:APPVEYOR_REPO_COMMIT_TIMESTAMP"
-Write-Host "$env:APPVEYOR_REPO_COMMIT_MESSAGE"
-Write-Host "$env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED"
-Write-Host "$env:APPVEYOR_SCHEDULED_BUILD"
-Write-Host "$env:APPVEYOR_FORCED_BUILD"
-Write-Host "$env:APPVEYOR_RE_BUILD"
-Write-Host "$env:APPVEYOR_RE_RUN_INCOMPLETE"
-Write-Host "$env:PLATFORM"
-Write-Host "$env:CONFIGURATION"
-Write-Host ""
-
 # Make sure we're using the Master branch and that it's not a pull request
 # Environmental Variables Guide: https://www.appveyor.com/docs/environment-variables/
 If ($env:APPVEYOR_REPO_BRANCH -ne 'master') {
@@ -68,19 +28,29 @@ Else {
     If ($res.FailedCount -eq 0) {
         # Publish the new version back to Master on GitHub
         Try {
-            # Set up a path to the git.exe cmd, import posh-git to give us control over git, and then push changes to GitHub
+            # Set up a path to the git.exe cmd, import posh-git to give us control over git
             $env:Path += ";$env:ProgramFiles\Git\cmd"
             Import-Module posh-git -ErrorAction Stop
+
+            # Configure the git environment
+            git config --global credential.helper store
+            Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:GitHubKey):x-oauth-basic@github.com`n"
+            git config --global user.email "$env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL"
+            git config --global user.name "$env:APPVEYOR_REPO_COMMIT_AUTHOR"
+            git config --global core.autocrlf true
+            git config --global core.safecrlf false
+
+            # Push changes to GitHub
             git checkout master
             git add --all
             git status
             git commit -s -m "AppVeyor validate: $env:APPVEYOR_BUILD_VERSION"
             git push origin master
-            Write-Host "$env:APPVEYOR_BUILD_VERSION published to GitHub." -ForegroundColor Cyan
+            Write-Host "$env:APPVEYOR_BUILD_VERSION pushed to GitHub." -ForegroundColor Cyan
         }
         Catch {
             # Sad panda; it broke
-            Write-Warning "Publishing update to GitHub failed."
+            Write-Warning "Push to GitHub failed."
             Throw $_
         }
     }

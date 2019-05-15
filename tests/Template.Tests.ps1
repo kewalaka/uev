cc7bbe8641abea4a4df54e3455126b87460ed856
Aaron Parker <aaron@stealthpuppy.com>
2019-05-15 17:42:51 +1000

    Add XML schema validation

--- tests/Template.Tests.ps1
+++ tests/Template.Tests.ps1
@@ -5,7 +5,16 @@
 
 # Variables
 $path = Join-Path $projectRoot "templates"
+$tests = Join-Path $projectRoot "tests"
+$schema = Join-Path $tests "SettingsLocationTemplate.xsd"
 $templates = Get-ChildItem -Path $path -Recurse -Include *.*
+
+# Export XML validation module
+. "$projectRoot\tests\Test-XmlSchema.psm1"
+
+# Echo paths
+Write-Host "Templates path: $templates"
+Write-Host "Tests path: $tests"
 
 #region Tests
 Describe "Template file type tests" {
@@ -26,6 +35,9 @@ Describe "Template XML format tests" {
                 Write-Warning "Failed to read $($template.Name)."
             }
             $content | Should -BeOfType System.Xml.XmlNode
+        }
+        It "$($template.Name) should validate against the schema" {
+            Test-XmlSchema -XmlPath $template.FullName -SchemaPath $schema | Should -Be $True
         }
     }
 }
